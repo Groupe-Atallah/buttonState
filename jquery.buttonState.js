17c77c222b5b3d89f7d87b4da57b83f2bf683dd0
Johann Troendle <johann.troendle@gmail.com>
2015-01-16 15:34:15 -0500

    Fixed call with several buttons

--- jquery.buttonState.js
+++ jquery.buttonState.js
@@ -2,30 +2,31 @@
 
   var methods = {
     deactivate : function( ) {      
-        var $el = $(this),
-            btnWidth = $el.data("width") ? $el.data("width") : $el.outerWidth();
-        if(methods.isDeactivated.call(this)) return true;
-        var content = ($el.find(".btnText").length) ? $el.find(".btnText").html() : $el.html(),
-          btnContent = "<span class='btnText' style='display:none;'>"+content+"</span>";
+        $(this).each(function() {
+          var $el = $(this),
+              btnWidth = $el.data("width") ? $el.data("width") : $el.outerWidth();
+          if($el.hasClass("loading")) return true;
+          var content = ($el.find(".btnText").length) ? $el.find(".btnText").html() : $el.html(),
+            btnContent = "<span class='btnText' style='display:none;'>"+content+"</span>";
 
-      $el
-        .data('width',btnWidth)
-        .addClass("loading")
-        .css("min-width", btnWidth)
-        .html(btnContent+"<span class='loader'><img src='/images/loading_white_on_black.gif' /></span>");
-        
+        $el
+          .data('width',btnWidth)
+          .addClass("loading")
+          .css("min-width", btnWidth)
+          .html(btnContent+"<span class='loader'><img src='/images/loading_white_on_black.gif' /></span>");
+        });
         return false;
     },
-    isDeactivated : function( ) {
-      return $(this).hasClass("loading");
-    },
-    activate : function( content ) {   
-      var $el = $(this); 
-      $el
-        .removeClass("loading")
-        .css("min-width", '')
-        .find(".loader").remove();
-      $el.find(".btnText").css("display","block");
+    
+    activate : function( content ) {
+      $(this).each(function() {
+        var $el = $(this); 
+        $el
+          .removeClass("loading")
+          .css("min-width", '')
+          .find(".loader").remove();
+        $el.find(".btnText").css("display","block");
+      });
     }
   };
 
